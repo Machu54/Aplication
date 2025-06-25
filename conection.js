2a6308821c637f7cc6ca3f6bdd3d5dca803afc42
Machu54 <carlos.machuca.24e@utzmg.edu.mx>
2025-06-25 13:25:23 -0600

    mongoapis

--- conection.js
+++ conection.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 
 const mongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(mongoUrl);
+mongoose.connect(mongoUrl, {
+
+});
 
 const collectionsh = new mongoose.Schema({
-  categoria: String,
-  garantia: String,
-  precio: Number,
-  marca: String
+  user_id: String,
+  jwt: String,
 }, { versionKey: false });
 
 module.exports = {
-  coleccionmodelo: mongoose.model("productos", collectionsh)
+  coleccionmodelo: mongoose.model("sessions", collectionsh)
 };
--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/productos', async (req, res) => {
+
+app.get('/api/productos', async (req, res) => {
   try {
     const docs = await mongo.coleccionmodelo.find({});
-    console.log(docs);
+        console.log(docs);
     res.json(docs);
   } catch (err) {
     console.error("Error al obtener productos:", err);
@@ -19,17 +20,58 @@ app.get('/productos', async (req, res) => {
   }
 });
 
-
-app.get('/productos/:id', async(req,res)=>{
-try{
- const docs = await mongo.coleccionmodelo.findById({});
- 
+app.get('/api/productos/:id', async (req, res) => {
+  try {
+        const id= req.params.id;
+    const docs = await mongo.coleccionmodelo.findOne({_id:id});
     res.json(docs);
-}catch(err){
-  console.error("Error al obtener productos:", err);
-res.status(500).json({error:'Error al consultar productos' });
-}
+  } catch (err) {
+    console.error("Error al obtener producto:", err);
+    res.status(500).json({ error: 'Error al consultar producto' });
+  }
+});
 
+
+app.post('/api/productosinser', async (req, res) => {
+  try {
+    const dato = req.body;
+    const docs = new mongo.coleccionmodelo(dato);
+    const resultado = await docs.save();
+    res.status(201).json(resultado);
+  } catch (err) {
+    console.error("Error al insertar producto:", err);
+    res.status(500).json({ error: 'Error al agregar producto' });
+  }
+});
+
+app.put('/api/productosup/:id',async(req,res)=>{
+  try{
+      const id = req.params.id;
+    const datos = req.body;
+
+    const docs = await mongo.coleccionmodelo.findByIdAndUpdate(
+      id,
+      { $set: datos },
+      { new: true }
+    );
+    res.json(docs);
+    } catch (err) {
+    console.error("Error al obtener producto:", err);
+    res.status(500).json({ error: 'Error al consultar producto' });
+}});
+
+
+
+
+app.delete('/api/productosdel/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const docs = await mongo.coleccionmodelo.findByIdAndDelete(id);
+    res.json(docs);
+    }catch (err) {
+    console.error("Error al eliminar producto:", err);
+    res.status(500).json({ error: 'Error al eliminar producto' });
+  }
 });
 
 app.listen(PORT, () => {
